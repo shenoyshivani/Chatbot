867a077b0f8ca276c7b25966764b6cecba374995
Shivani Shenoy <164753825+shenoyshivani@users.noreply.github.com>
2025-09-13 08:39:34 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+
+  {
+    files: ["**/*.{js,jsx}"],
+    excludedFiles: ["server/**/*.js"], 
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser, 
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react/prop-types": "off",
+    },
+  },
+ 
+  {
+    files: ["server/**/*.js", "index.js"], 
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.node, 
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      
+    },
+  },
+]);
